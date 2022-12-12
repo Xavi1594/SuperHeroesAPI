0a24827101f2f2e527477416a7f1a49fb078886f
Manu-Estrada <mirucuxusticieru@gmail.com>
2022-12-12 13:00:49 +0100

    Header enlazado

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <HeaderOne/>
+</template>
+<script>
+import HeaderOne from './components/home/HeaderOne.vue';
+export default {
+name:"App",
+components:{
+    HeaderOne,
+}    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/components/home/HeaderOne.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+    <h1>SUPER HEROES UNIVERSE</h1>
+    </header>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
+
