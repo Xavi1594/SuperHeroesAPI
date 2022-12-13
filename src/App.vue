e475aef6e4a4fcde320648d5890a8be4b35fdf64
guillermo <h.uhm292@gmail.com>
2022-12-13 13:45:41 +0100

    footer-done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <HeaderOne/>
+  <FootPage/>
 </template>
 <script>
+import FootPage from './components/home/FootPage.vue';
 import HeaderOne from './components/home/HeaderOne.vue';
 export default {
 name:"App",
 components:{
     HeaderOne,
+    FootPage
 }    
 }
 </script>
---
+++ src/components/home/FootPage.vue
@@ -0,0 +1,56 @@
+<template>
+    <footer class="footer">
+    
+        <div class="footer__container-title">
+            <h2 class="footer__title">SUPER HEROES UNIVERSE</h2>
+        </div>
+        <div class="footer__container-social-media">
+            <i class="fa-brands fa-instagram icon-media"></i>
+            <i class="fa-brands fa-twitter icon-media"></i>
+            <i class="fa-brands fa-facebook icon-media"></i>
+       
+        </div>
+        
+    </footer>
+    
+  
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+.footer {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: auto;
+    background: linear-gradient(90deg, rgba(0, 0, 0, 1) 0%, rgba(182, 8, 8, 1) 99%);
+        background-color: rgb(0, 0, 0);
+        padding: 1rem;
+
+    .footer__title{
+        font-family: Arial, Helvetica, sans-serif;
+        color: white;
+        font-size: 2rem;
+        
+
+    }
+
+    .footer__container-social-media{
+        .icon-media{
+            font-size: 1.7rem;
+            color: white;
+            margin-right: 1rem;
+            cursor: pointer;
+            &:hover{
+                opacity: .5;
+            }
+    }
+}
+}
+
+    
+</style>
+
