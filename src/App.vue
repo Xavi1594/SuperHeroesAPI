f3d1e10b89997e0e1c8856a71b1d79d388347dee
Xavi1594 <xavi9415@hotmail.com>
2022-12-13 14:47:36 +0100

    esqueleto galeria imagenes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <HeaderOne/>
+  <HeaderOne/> 
+  <main>
+  <Card/> 
+  <Card/><Card/> <Card/> <Card/> <Card/> <Card/> <Card/> 
+</main>
 </template>
 <script>
+import Card from './components/home/Card.vue';
 import HeaderOne from './components/home/HeaderOne.vue';
 export default {
 name:"App",
 components:{
     HeaderOne,
+    Card
 }    
 }
 </script>
-<style lang="">
-    
+<style lang="scss">
+main {
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  // width: 90%;
+ text-align: center;
+ justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+
 </style>
---
+++ src/components/home/Card.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="card__container">
+        <figure class="card__img">
+
+            <!-- <img :src="getImage" :alt="card"> -->
+        </figure>
+        <div class="card__properties">
+            <p class="properties__name"> name Nombre</p>
+            <div class="properties__container">
+                <p>race</p>
+                <p>power</p>
+                <p>alinement</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+.card__container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 12em;
+    height: 18em;
+    margin-bottom: 1em;
+    // background-color: chartreuse;
+    justify-self:center ;
+
+    .card__img {
+        width: 100%;
+        height: 13em;
+        background-color: red;
+    }
+
+    .card__properties {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 90%;
+         height: 5em;
+    }
+
+    .properties__container {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        margin-top: 1rem;
+    }
+}
+</style>
+
+  
