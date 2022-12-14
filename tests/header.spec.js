bb277551be2265322067848b8c8a7b748b21507e
JehiselRuth <jehisel.rrp@gmail.com>
2022-12-14 13:01:40 +0100

    test header links

--- tests/header.spec.js
+++ tests/header.spec.js
@@ -1,12 +1,30 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HeaderOne from '../home/HeaderOne.vue'
+import HeaderOne from '../src/components/home/HeaderOne.vue'
 
 describe('Header', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(HeaderOne)
+ })
   it('should title h1', () => {
-    const wrapper = mount(HeaderOne)
+    
     const value = wrapper.find('h1').text()
     expect(value).toBe('SUPER HEROES UNIVERSE')
   })
-})
+
+  it('should first link include Home'), () => {
+    const value = wrapper.find('a').text()
+    expect(value).toBe('Home')
+  }
+
+  it('should second link include Favorites'), () => {
+    const value = wrapper.findAll('a')[1].text()
+    expect(value).toBe('Favorites')
+ }
+
+}
+)
+
