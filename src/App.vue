db4d4a6b446550d421d675ab83a6b1e72d51f796
JehiselRuth <jehisel.rrp@gmail.com>
2022-12-19 12:25:43 +0100

    cambio de rutas

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,15 @@
 <script setup>
-import { onBeforeMount, reactive } from "vue";
-import { useHeroeStore } from "./stores/store";
 
-import HeaderPage from "./components/home/HeaderPage.vue";
-import CardItem from "./components/home/CardItem.vue";
-import FooterPage from "./components/home/FooterPage.vue";
 
-const heroesStore = useHeroeStore();
+import HomeView from "./views/HomeView.vue";
 
-onBeforeMount(() => {
-  getHeroes();
-});
-
-// METHODS
-const getHeroes = async () => {
-  await heroesStore.fetchHeroes();
-};
 </script>
 
 <template>
-  <HeaderPage />
+  <HomeView />
 
-  <main>
-    <CardItem
-      v-for="heroe in heroesStore.heroes.slice(148, 168)"
-      :key="heroe.id"
-      :id="heroe.id"
-      :img="heroe.images.lg"
-      :name="heroe.name"
-      :speed="heroe.powerstats.speed"
-      :power="heroe.powerstats.power"
-      :strength="heroe.powerstats.strength"
-    />
-  </main>
-  <FooterPage />
+  
+ 
 </template>
 
 <style lang="scss">
@@ -41,15 +17,5 @@ body {
   background-image: url(./assets/FondoHome3.png);
 }
 
-main {
-  border-radius: 10px;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  width: 60%;
-  text-align: center;
-  justify-content: center;
-  align-items: center;
-  margin: 0 auto;
-  gap: 1em;
-}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,58 @@
 <script setup>
-import HeaderPage from "./HeaderPage"
+import HeaderPage from "../components/home/HeaderPage.vue"
+import FooterPage from "../components/home/FooterPage.vue"
+import CardItem from "../components/home/CardItem.vue"
+
+
+import { onBeforeMount, reactive } from "vue";
+import { useHeroeStore } from "../stores/store.js";
+
+
+
+const heroesStore = useHeroeStore();
+
+onBeforeMount(() => {
+  getHeroes();
+});
+
+// METHODS
+const getHeroes = async () => {
+  await heroesStore.fetchHeroes();
+};
+
 
 
 </script>
 
 <template>
+    <HeaderPage />
   <main>
-    <SingleComponent />
-  </main>
+    <CardItem
+      v-for="heroe in heroesStore.heroes.slice(148, 168)"
+      :key="heroe.id"
+      :id="heroe.id"
+      :img="heroe.images.lg"
+      :name="heroe.name"
+      :speed="heroe.powerstats.speed"
+      :power="heroe.powerstats.power"
+      :strength="heroe.powerstats.strength"
+    />
+</main>
+
+    <FooterPage />
 </template>
+
+<style lang="scss">
+
+main {
+  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  width: 60%;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  gap: 1em;
+}
+</style>
