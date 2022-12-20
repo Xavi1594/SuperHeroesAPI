e6c840072bc7e1f67a713601a267246c0b601710
Xavi1594 <xavi9415@hotmail.com>
2022-12-20 14:26:48 +0100

    añadiendo estilos de la lista

--- src/components/favorites/Listheroes.vue
+++ src/components/favorites/Listheroes.vue
@@ -1,14 +1,37 @@
 <template>
-    <div class="favorites__list-container">
-        <h2 class="list__title">Add your favorite Super Hero</h2>
-      <div class="list__container">
-        <ul>
-            <li>5</li>
-            <li>44</li>
-            <li>44</li>
-        </ul>
-      </div>
-      </div>
-  </template>
+  <div class="favorites__list-container">
+    <h2 class="list__title">Add your favorite Super Hero</h2>
+    <div class="list__container">
+      <ul>
+        <li>Paco Perez</li>
+        <li>Mariano Rajoy</li>
+        <li>Super Lopez</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+ .favorites__list-container {
+  position: absolute;
+  right: 5em;
+    display: flex;
+    width: 20%;
+    background-color: red;
+    flex-direction: column;
+  }
+    .list__title {
+      font-size: 20px;
+      font-weight: bold;
+    }
+    ul {
+      display: flex;
+      flex-direction: column;
+    }
+
+</style>
   
   
+  
+
+
+
