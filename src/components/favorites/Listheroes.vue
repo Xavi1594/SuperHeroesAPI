b69cc6370b659301be254d24a77230f32872a5b7
Xavi1594 <xavi9415@hotmail.com>
2022-12-20 15:25:27 +0100

    estilos de la caja de favoritos

--- src/components/favorites/Listheroes.vue
+++ src/components/favorites/Listheroes.vue
@@ -13,30 +13,53 @@
 <style lang="scss">
  .favorites__list-container {
   position: absolute;
-  right: 5em;
+  right: 1em;
+  top: 8.8em;
     display: flex;
-    width: 20%;
-    height: 30%;
-    background-color: red;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 15%;
+    height: 35%;
+     padding: 0.5em;
+    background: linear-gradient(
+      90deg,
+      rgba(0, 0, 0, 1) 0%,
+      rgba(182, 8, 8, 1) 99%
+    );
+    background-color: rgb(0, 0, 0);
     flex-direction: column;
+    border-radius: 10px;
   }
     .list__title {
       font-size: 20px;
       font-weight: bold;
+      padding-bottom: 0.5em ;
       color: white;
       text-align: center;
+    
     }
     .list__container {
-      background-color: red;
-      padding: 1em;
-    }
+      height: 50%;
+      width: 90%;
       
+      
+      
+    }
+    
     ul {
       display: flex;
       flex-direction: column;
-      background-color: white;
-      padding: 1em;
+      background-color:gainsboro ;
+      height: 100%;
+      border-radius: 10px;
       
+     li {
+         width: 100%;
+         padding-top: 0.5em;
+         display: flex;
+         flex-direction: column;
+         font-family: Arial, Helvetica, sans-serif;
+     }
     }
 
 </style>
--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -2,6 +2,7 @@
 import HeaderPage from "../components/HeaderPage.vue";
 import FooterPage from "../components/FooterPage.vue";
 import CardItem from "../components/CardItem.vue";
+import ListheroesVue from "../components/favorites/Listheroes.vue";
 import { useHeroeFavoriteStore } from "@/stores/storeFavorites";
 
 const favoriteStore = useHeroeFavoriteStore();
@@ -11,12 +12,14 @@ const favoriteStore = useHeroeFavoriteStore();
 <template>
   <HeaderPage />
 
+
 <main>
   <CardItem
     v-for="heroe in favoriteStore.heroesFavorites"
     :key="heroe.id"
     :heroe="heroe"
   />
+  <ListheroesVue />
 </main>
 
   <FooterPage />
