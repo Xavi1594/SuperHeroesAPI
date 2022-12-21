0dfc046953423e0f80a164f459467e4a91852856
JehiselRuth <jehisel.rrp@gmail.com>
2022-12-21 14:10:01 +0100

    estrellas en cards

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -4,59 +4,53 @@ import FooterPage from "../components/FooterPage.vue";
 import CardItem from "../components/CardItem.vue";
 import ListheroesVue from "../components/favorites/Listheroes.vue";
 import { useHeroeFavoriteStore } from "@/stores/storeFavorites";
+import StarsRating from "../components/favorites/StarsRating.vue";
 
 const favoriteStore = useHeroeFavoriteStore();
-
-
-
 </script>
 <template>
   <HeaderPage />
 
-
-<main>
-  <CardItem
-  v-for="heroe in favoriteStore.heroesFavorites"
-  :key="heroe.id"
-  :heroe="heroe"
-  />
-  <div v-if="favoriteStore.heroesFavorites.length > 0">
+  <main>
+    <div v-for="heroe in favoriteStore.heroesFavorites" :key="heroe.id">
+    <StarsRating />
+      <CardItem :heroe="heroe" />
     </div>
+    <div v-if="favoriteStore.heroesFavorites.length > 0"></div>
     <div v-else>
       <p class="card__p">No has añadido ningún heroe a favoritos</p>
     </div>
-  <ListheroesVue />
-</main>
-<div class="containerFooter">
-  <FooterPage />
+    <ListheroesVue />
+  </main>
+  <div class="containerFooter">
+    <FooterPage />
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   main {
-  border-radius: 10px;
-  display: grid;
-  grid-template-columns: repeat(auto, 1fr);
-  width: 60%;
-  text-align: center;
-  justify-content: center;
-  align-items: center;
-  margin: 0 auto;
-  gap: 1em;
-}
+    border-radius: 10px;
+    display: grid;
+    grid-template-columns: repeat(auto, 1fr);
+    width: 60%;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    gap: 1em;
+  }
 }
 
-.containerFooter  {
-  
-  position:absolute;
+.containerFooter {
+  position: absolute;
   bottom: 0;
   width: 100%;
 }
 
 .card__p {
- position: absolute;
- left: 30%;
+  position: absolute;
+  left: 30%;
   width: 40%;
   padding: 20px;
   text-align: center;
@@ -72,6 +66,4 @@ const favoriteStore = useHeroeFavoriteStore();
   color: aqua;
   box-shadow: 0 4px 16px rgba(49, 138, 172, 1);
 }
-
-
 </style>
