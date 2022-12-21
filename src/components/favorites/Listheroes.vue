56d75e4d8a261b8d91b95c2e1d8857b045783c59
guillermo <h.uhm292@gmail.com>
2022-12-21 09:38:22 +0100

    recuperar cambios

--- src/components/favorites/Listheroes.vue
+++ src/components/favorites/Listheroes.vue
@@ -3,12 +3,23 @@
         <h2 class="list__title">Add your favorite Super Hero</h2>
       <div class="list__container">
         <ul>
-            <li>5</li>
-            <li>44</li>
-            <li>44</li>
+            <li>Firulais</li>
+            <li>Bob Esponja</li>
+            <li>Chiquito de la calzada</li>
         </ul>
       </div>
       </div>
   </template>
+  <style lang="scss">
+  .favorites__list-container{
+    background-color: red;
+    
+    position: absolute;
+    left:10em;
+  }
+</style>
+  
+
+ 
   
   
